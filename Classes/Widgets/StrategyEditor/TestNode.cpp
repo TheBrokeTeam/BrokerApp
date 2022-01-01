c3d020bc76fa9a7c9ee76f002d90f01e9fb8af86
tatolevicz <leviczios@gmail.com>
2022-01-01 20:09:27 -0300

    custom color node

--- Classes/Widgets/StrategyEditor/TestNode.cpp
+++ Classes/Widgets/StrategyEditor/TestNode.cpp
@@ -3,7 +3,10 @@
 //
 
 #include "TestNode.h"
+#include <imgui.h>
 #include <imnodes.h>
+
+#include "../../Editor.h"
 
 TestNode::TestNode() {
     _id = ++current_id;
@@ -13,11 +16,18 @@ TestNode::TestNode() {
 
 void TestNode::render() {
 
+    ImNodes::PushColorStyle(ImNodesCol_TitleBar, ImGui::ColorConvertFloat4ToU32(Editor::broker_yellow_active));
+    ImNodes::PushColorStyle(ImNodesCol_TitleBarHovered, ImGui::ColorConvertFloat4ToU32(Editor::broker_yellow_hover));
+    ImNodes::PushColorStyle(ImNodesCol_TitleBarSelected, ImGui::ColorConvertFloat4ToU32(Editor::broker_yellow));
+
     ImNodes::BeginNode(_id);
 
     ImNodes::BeginNodeTitleBar();
+    ImGui::PushStyleColor(ImGuiCol_Text,Editor::broker_black);
     ImGui::Text("simple node :)");
+    ImGui::PopStyleColor();
     ImNodes::EndNodeTitleBar();
+
 
     ImNodes::BeginInputAttribute(_idInput);
     ImGui::Text("input");
@@ -31,6 +41,10 @@ void TestNode::render() {
     ImNodes::EndOutputAttribute();
 
     ImNodes::EndNode();
+    ImNodes::PopColorStyle();
+    ImNodes::PopColorStyle();
+    ImNodes::PopColorStyle();
+
 
 }
 
