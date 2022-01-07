470282b8952c379bb62665a874ec4f85bf12eeae
tatolevicz <leviczios@gmail.com>
2022-01-07 10:35:25 -0300

    tooltips improved

--- Classes/Widgets/NodesList.h
+++ Classes/Widgets/NodesList.h
@@ -29,27 +29,27 @@ public:
             switch (type) {
                 case UiNodeType::CROSS:
                     icon = Editor::Icons::node_cross;
-                    desc = "The output is true only when the A crosses B (up or down), otherwise stays false. Use case: used for cross SMA triggers.";
+                    desc = "Cross Node: the output is true only when the A crosses B (up or down), otherwise stays false.\nUse case: used for cross SMA triggers.";
                     break;
                 case UiNodeType::COUNTER:
                     icon = Editor::Icons::node_counter;
-                    desc = "Count each time A is different from zero. Use sase: use for count cross from Cross Node.";
+                    desc = "Counter Node: count each time A is different from zero.\nUse case: use for count cross from Cross Node.";
                     break;
                 case UiNodeType::TREND:
                     icon = Editor::Icons::node_trend;
-                    desc = "The output is true when is trending (down or up),otherwise stays false. Use case: combine with cross SMA.";
+                    desc = "Trend Node: the output is true when is trending (down or up),otherwise stays false.\nUse case: combine with cross SMA.";
                     break;
                 case UiNodeType::TRADE:
                     icon = Editor::Icons::node_trade;
-                    desc = "When A is true or more than 0, open a position (shorting or longing).";
+                    desc = "Trade Node: when A is true or more than 0, open a position (shorting or longing).";
                     break;
                 case UiNodeType::BAR_SEQ_DOWN:
                     icon = Editor::Icons::node_bar_seq_down;
-                    desc = "Identify N numbers of negative bars.";
+                    desc = "Down Sequence Node: Identify N numbers of negative bars in sequence.";
                     break;
                 case UiNodeType::BAR_SEQ_UP:
                     icon = Editor::Icons::node_bar_seq_up;
-                    desc = "Identify N numbers of positive bars.";
+                    desc = "Up Sequence Node: Identify N numbers of positive bars in sequence.";
                     break;
                 default:
                     break;
